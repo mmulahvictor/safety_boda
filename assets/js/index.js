b35bc64685bdcd7a7abab15adcdd1a09133adc2f
borebrian2021 <borebrian2021@gmail.com>
2022-05-18 00:09:00 -0700

    Added the javascript validation

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+const regCollection = document.getElementsByClassName("input2");
+var form = document.getElementById("registration");
+function handleForm(event) { 
+    alert('qwkjdhfgsjkl');
+    
+    
+    
+    event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+function validateInputs(){
+    // for (let x in regCollection){
+    // if(x.value)
+    
+    // }
+}
+
