4329290633f7e8a69cb84e0ededd4a513f013401
borebrian2021 <borebrian2021@gmail.com>
2022-05-18 13:13:50 -0700

    Added the  error response

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,21 +2,33 @@ const regCollection = document.getElementsByClassName("input2");
 var span = document.getElementById("close");
 var modal = document.getElementById("myModal");
 var form = document.getElementById("registration");
-var regForm={};
-var x=localStorage.getItem("vehicleDB");
-var vehicleRecord=[];
+var regForm = {};
+var x = localStorage.getItem("vehicleDB");
+var vehicleRecord = [];
 var reset = document.getElementById("reset");
-span.onclick=function(){
-    modal.style.display = "none";
+span.onclick = function () {
+  modal.style.display = "none";
 
 }
 
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+}
+
+
+function success(message){
+  document.getElementById("ok").style.visibility = "visible";
+  document.getElementById("wrong").style.visibility = "hidden";
+  document.getElementById('response').innerHTML=message;
+}
+function error(message){
+  document.getElementById("ok").style.visibility = "hidden";
+  document.getElementById("wrong").style.visibility = "visible";
+  document.getElementById('response').innerHTML=message;
+}
 
 
 
@@ -33,84 +45,79 @@ window.onclick = function(event) {
 
 
 
-
-
-if(x!=null){
-    vehicleRecord=JSON.parse(localStorage.getItem("vehicleDB"));
-        console.log(vehicleRecord);
+if (x != null) {
+  vehicleRecord = JSON.parse(localStorage.getItem("vehicleDB"));
+  console.log(vehicleRecord);
 
 
 }
 // localStorage.removeItem('vehicleDB')
 
-function handleForm(event) { 
-var fname= document.getElementById('fname').value;
-var plate= document.getElementById('plate').value;
-var email= document.getElementById('email').value;
-var chasis= document.getElementById('chasis').value;
-var password= document.getElementById('password').value;
+function handleForm(event) {
+  var fname = document.getElementById('fname').value;
+  var plate = document.getElementById('plate').value;
+  var email = document.getElementById('email').value;
+  var chasis = document.getElementById('chasis').value;
+  var password = document.getElementById('password').value;
 
-var model= document.getElementById('model').value;
-var manufacturer= document.getElementById('manufacturer').value;
-var oparation= document.getElementById('oparation').value;
-var idnumber= document.getElementById('phone').value;
-// CHECK IF VEGICLE EXISTS
+  var model = document.getElementById('model').value;
+  var manufacturer = document.getElementById('manufacturer').value;
+  var oparation = document.getElementById('oparation').value;
+  var idnumber = document.getElementById('phone').value;
+  // CHECK IF VEGICLE EXISTS
 
-if(vehicleRecord.length==0){
-    
+  if (vehicleRecord.length == 0) {
+
     var regForm = {
-        fname1:fname,
-        plate1:plate,
-        email1:email,
-        chasis1:chasis,
-        model1:model,
-        oparation1:oparation,
-        manufacturer1:manufacturer,
-        idnumber1:idnumber,
-        password:password
-        };
-        vehicleRecord.push(regForm)
-        localStorage.setItem("vehicleDB", JSON.stringify(vehicleRecord)); //store colors
-        var fromDB = JSON.parse(localStorage.getItem("vehicleDB")); //get them back
-        console.log(fromDB);
-}
-else{
-for(var i=0 ; i< vehicleRecord.length; i++){
-  if(vehicleRecord[i].plate1==plate){
-      alert("This vehicle already exists")
-      modal.style.display = "block";
-     
+      fname1: fname,
+      plate1: plate,
+      email1: email,
+      chasis1: chasis,
+      model1: model,
+      oparation1: oparation,
+      manufacturer1: manufacturer,
+      idnumber1: idnumber,
+      password: password
+    };
+    vehicleRecord.push(regForm)
+    localStorage.setItem("vehicleDB", JSON.stringify(vehicleRecord)); //store colors
+    var fromDB = JSON.parse(localStorage.getItem("vehicleDB")); //get them back
+    console.log(fromDB);
   }
-  else{
+  else {
+    for (var i = 0; i < vehicleRecord.length; i++) {
+      if (vehicleRecord[i].plate1 == plate) {
+        
+
+
+        modal.style.display = "block";
+
+      }
+      else {
 
         var regForm = {
-        fname1:fname,
-        plate1:plate,
-        email1:email,
-        chasis1:chasis,
-        model1:model,
-        oparation1:oparation,
-        manufacturer1:manufacturer,
-        idnumber1:idnumber,
+          fname1: fname,
+          plate1: plate,
+          email1: email,
+          chasis1: chasis,
+          model1: model,
+          oparation1: oparation,
+          manufacturer1: manufacturer,
+          idnumber1: idnumber,
         };
         vehicleRecord.push(regForm)
         localStorage.setItem("vehicleDB", JSON.stringify(vehicleRecord)); //store colors
         var fromDB = JSON.parse(localStorage.getItem("vehicleDB")); //get them back
         console.log(fromDB);
         reset.click();
-      modal.style.display = "block";
+  
 
+        modal.style.display = "block";
+
+      }
+    }
   }
+  event.preventDefault();
 }
-}
-
-
-
-
-
-    
-    
-    event.preventDefault(); 
-} 
 form.addEventListener('submit', handleForm);
 
